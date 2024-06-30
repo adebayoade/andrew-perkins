156137c4885384fa7ab962bb94626694f5af8dc0
Adebayo Ade <vanbayor@gmail.com>
2024-06-30 13:59:06 +0100

    updated footer copy

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -2,8 +2,11 @@ export default function Footer() {
   return (
     <footer>
       <div className="mt-14 py-10 bg-[#292929]">
-        <div className="container flex flex-col gap-5 text-white">
+        <div className="container flex flex-col gap-3 text-white">
           <p className="text-center">&copy; 2024 - Andrew Perkins Media. All rights reserved.</p>
+          <span className="text-center text-sm">
+            Designed by <a className="hover:text-orange-500 text-orange-400" href="https://leetano.com">Leetano</a>
+          </span>
         </div>
       </div>
     </footer>
